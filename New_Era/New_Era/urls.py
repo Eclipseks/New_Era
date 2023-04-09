e0d5c7941526d13a360c7522d85e938390d4cf5f
Eclipseks <artur.viktor68@gmail.com>
2023-04-09 21:44:25 +0300

    09.04.2023

--- New_Era/New_Era/urls.py
+++ New_Era/New_Era/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 from main import views
 
 urlpatterns = [
-    path('home/', views.upload_list, name='upload_list'),
+    path('', views.upload_list, name='upload_list'),
     path('upload_file/', views.upload_file, name='upload_file'),
     path('admin/', admin.site.urls),
-    path('', views.login_view, name='login'),
+    path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('export/', views.export_view, name='export'),
 ]
--- New_Era/main/views.py
+++ New_Era/main/views.py
@@ -1,121 +1,127 @@
 from datetime import datetime
 import os
 
-from django.http import HttpResponseRedirect, HttpResponseNotAllowed
-from django.urls import reverse
+import openpyxl
+
+from django.http import HttpResponse
 from .forms import UploadFileForm
 import pandas as pd
+from django.contrib import messages
 from .models import Upload
+from django.db.models import Q
+from dateutil.parser import parse
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
-from django.contrib import messages
 from django.views.decorators.csrf import csrf_protect
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.cache import never_cache
 
 
-# Autentification page
+
 @csrf_protect
 def login_view(request):
+    if request.user.is_authenticated:
+        return redirect('/')  # replace 'home' with the name of the main page for authenticated users
+
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
-        # User.objects.create_user(username=username, email='artur.dima68@gmail.com', password=password)
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('upload_list')  # замените 'home' на имя вашей главной страницы
+            return redirect('upload_list')  # replace 'upload_list' with the name of your desired page
         else:
             messages.error(request, 'Неверные имя пользователя или пароль')
     return render(request, 'main/login.html')
 
 
 # Table on main page
-@login_required()
+@never_cache
+@login_required(login_url='/login/')
 def upload_list(request):
-    # get the sorting parameter from the URL
-    sort = request.GET.get('sort')
+    files = Upload.objects.order_by('date_upload')
 
-    # set the default sorting
-    if sort == None or sort == 'DateUpload':
-        files = Upload.objects.order_by('date_upload')
-    elif sort == 'Firstname':
-        files = Upload.objects.order_by('first_name')
-    elif sort == 'Lastname':
-        files = Upload.objects.order_by('last_name')
-    elif sort == 'Email':
-        files = Upload.objects.order_by('email')
-    elif sort == 'Brend':
-        files = Upload.objects.order_by('brand')
-    elif sort == 'Country':
-        files = Upload.objects.order_by('country')
-    elif sort == 'DateRegistr':
-        files = Upload.objects.order_by('date_registr')
-    elif sort == 'Mpc1':
-        files = Upload.objects.order_by('mpc1')
-    elif sort == 'Mpc2':
-        files = Upload.objects.order_by('mpc2')
-    elif sort == 'Mpc3':
-        files = Upload.objects.order_by('mpc3')
-    elif sort == 'Mpc4':
-        files = Upload.objects.order_by('mpc4')
-    elif sort == 'Phone':
-        files = Upload.objects.order_by('phone')
-    
     return render(request, 'main/upload_list.html', {'files': files})
 
         
 
 # Import data from exel, csv
+@never_cache
 @login_required
 def upload_file(request):
+    duplicate_entries = []
+
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
             files = request.FILES.getlist('file')
             for file in files:
-                # Обработка каждого файла в списке
-                file_extension = os.path.splitext(file.name)[1]
-                if file_extension == '.xls' or file_extension == '.xlsx':
-                    df = pd.read_excel(file)
-                elif file_extension == '.csv':
-                    df = pd.read_csv(file)
-                else:
-                    continue  # Неизвестный формат файла
+                duplicates = process_file(file)
+                duplicate_entries.extend(duplicates)
 
-                for index, row in df.iterrows():
-                    try:
-                        try:
-                            date = row['date_registr'].split(' ')
-                            if len(date) == 2:
-                                date_reg = date[0]
-                            elif len(date) == 1:
-                                date = row['date_registr']
-                            else:
-                                raise Exception
-                        except:
-                            date = '1999-11-10'
-                        upload = Upload(
-                            first_name=row['first_name'],
-                            last_name=row['last_name'],
-                            phone=row['phone'],
-                            email=row['email'],
-                            brand=row['brand'],
-                            country=row['country'],
-                            date_registr=date,
-                            date_upload=datetime.now(),
-                            mpc1=row['mpc1'],
-                            mpc2=row['mpc2'],
-                            mpc3=row['mpc3'],
-                            mpc4=row['mpc4']
-                        )
-                        upload.save()
-                    except:
-                        continue
-
-            return HttpResponseRedirect(reverse('upload_list'))
+            return render(request, 'main/duplicates.html', {'duplicates': duplicate_entries})
     else:
         form = UploadFileForm()
     return render(request, 'main/upload_file.html', {'form': form})
+
+
+
+def process_file(file):
+    file_extension = os.path.splitext(file.name)[1]
+    if file_extension in ('.xls', '.xlsx'):
+        df = pd.read_excel(file)
+    elif file_extension == '.csv':
+        df = pd.read_csv(file)
+    else:
+        return  # Unknown file format
+
+    columns_operations = [
+        ('date_registr', lambda x: parse(str(x)).strftime("%Y-%m-%d")),
+        ('email', lambda x: 'example@gmail.com' if len(str(x).split('@')[1].split('.')) != 2 else x),
+        ('mpc1', lambda x: 0.00 if not isinstance(x, float) else x),
+        ('mpc2', lambda x: 0.00 if not isinstance(x, float) else x),
+        ('mpc3', lambda x: 0.00 if not isinstance(x, float) else x),
+        ('mpc4', lambda x: 0.00 if not isinstance(x, float) else x),
+    ]
+
+    upload_fields = set(field.name for field in Upload._meta.get_fields())
+    
+    duplicates = []
+
+    for _, row in df.iterrows():
+        upload_data = {col: op(row[col]) for col, op in columns_operations}
+        upload_data.update({col: row[col] for col in row.index if col not in dict(columns_operations).keys()})
+        upload_data = {col: val for col, val in upload_data.items() if col in upload_fields}
+
+        first_name = upload_data['first_name']
+        last_name = upload_data['last_name']
+        phone = upload_data['phone']
+        email = upload_data['email']
+        brand = upload_data['brand']
+        country = upload_data['country']
+
+        is_duplicate = Upload.objects.filter(
+            Q(first_name=first_name) &
+            Q(last_name=last_name) &
+            Q(phone=phone) &
+            Q(email=email) &
+            Q(brand=brand) &
+            Q(country=country)
+        ).exists()
+
+        if not is_duplicate:
+            upload_data['date_upload'] = datetime.now()
+            upload = Upload(**upload_data)
+            upload.save()
+            
+        if is_duplicate:
+            duplicates.append(upload_data)
+        else:
+            upload_data['date_upload'] = datetime.now()
+            upload = Upload(**upload_data)
+            upload.save()
+
+    return duplicates
 
 
 @login_required
@@ -123,9 +129,6 @@ def logout_view(request):
     logout(request)
     return redirect('/')
 
-
-from django.http import HttpResponse
-import openpyxl
 
 @login_required
 def export_view(request):
@@ -164,6 +167,6 @@ def export_view(request):
 
     # Сохранение Excel-файла и отправка пользователю
     response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
-    response['Content-Disposition'] = 'attachment; filename="my_excel_file.xlsx"'
+    response['Content-Disposition'] = f'attachment; filename="leed_{datetime.now().date()}.xlsx"'
     workbook.save(response)
     return response
